c0432f9aea6321cae22dfda49301970ec3486fb6
Vadim Godunko <vgodunko@gmail.com>
2025-09-29 09:59:47 +0400

    ESP32S3 demo

---
+++ example/esp32s3/source/main.adb
@@ -0,0 +1,20 @@
+
+pragma Ada_2022;
+
+with A0B.Types;
+
+procedure Main is
+   use type A0B.Types.Unsigned_32;
+
+   function F return String;
+
+   function F return String is ("def");
+
+   Count : A0B.Types.Unsigned_32 := 0;
+   S     : String := "abc" & F;
+
+begin
+   for J in 1 .. 1_000 loop
+      Count := @ + 1;
+   end loop;
+end Main;
